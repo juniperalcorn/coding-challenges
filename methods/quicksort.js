9a4174cfbf14e95adbee9bee7faf072664c7dcbf
junipera <juniper.alcorn@gmail.com>
2019-08-13 09:44:31 -0400

    fix quicksort

--- methods/quicksort.js
+++ methods/quicksort.js
@@ -1,19 +1,23 @@
 test = [8, 3, 5, 4, 7, 6, 1, 2]
 
 const pivot = (arr, startIdx = 0, endIdx = arr.length -1) =>{
+    
+    //needed swapping function, was maybe not getting it right on my own
+    const swap = (arr, idx1, idx2) =>{
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+    }
+    
+    
     let pivot = arr[startIdx]
     let pivotIdx = startIdx
 
-    for (let i = startIdx ; i<arr.length; i++){
+    for (let i = startIdx+1 ; i<=endIdx; i++){
         if (pivot > arr[i]){
             pivotIdx ++
-            let temp = arr[pivotIdx]
-            arr[i] = temp
-            temp = arr[i] 
+            swap(arr, pivotIdx, i)
         }
     }
-    pivot = arr[pivotIdx]
-    arr[pivotIdx] = pivot
+    swap(arr, startIdx, pivotIdx)
     
     // console.log(arr)
     return pivotIdx
